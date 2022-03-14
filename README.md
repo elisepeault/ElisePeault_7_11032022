2e45927fcf0f9e516f03482c14b9f50dda2e56dc
Péault <elise.peault2@gmail.com>
2022-03-14 18:05:03 +0100

    Basic backend structure (Node/Express)

---
+++ README.md
@@ -0,0 +1,1 @@
+Projet 7 - Formation "Développeur Web" - Openclassroom
---
+++ backend/server.js
@@ -0,0 +1,55 @@
+// IMPORT
+const http = require('http');    // import the HTTP Node's package 
+const app = require('./app');
+
+// returns a valid port (as a number or a string)
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+// Searches for errors and handles them appropriately.
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+// Use the HTTP Node's package to create a server
+const server = http.createServer(app);
+
+// Save the errorHandler function in the server
+server.on('error', errorHandler);
+
+// Event listener logging the port the server is running on in the console 
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+// Tell the server on which port it will run (function port) 
+server.listen(port);
